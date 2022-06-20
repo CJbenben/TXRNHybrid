47423f9fa370783cc00f5a4b438c6e8e095960fb
chenxiaojie <chenjie003@qq.com>
2022-06-20 17:17:37 +0800

    RN 混合开发demo 完成

--- ios/TXRNTest/ViewController.m
+++ ios/TXRNTest/ViewController.m
@@ -6,6 +6,7 @@
 //
 
 #import "ViewController.h"
+#import "RNPageController.h"
 
 @interface ViewController ()
 
@@ -16,7 +17,37 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor cyanColor];
 }
 
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    RNPageController *vc = [[RNPageController alloc] init];
+    [self.navigationController pushViewController:vc animated:YES];
+    
+//    NSLog(@"High Score Button Pressed");
+//    NSURL *jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.bundle?platform=ios"];
+//
+//    RCTRootView *rootView =
+//      [[RCTRootView alloc] initWithBundleURL: jsCodeLocation
+//                                  moduleName: @"RNHighScores"
+//                           initialProperties:
+//                             @{
+//                               @"scores" : @[
+//                                 @{
+//                                   @"name" : @"Alex",
+//                                   @"value": @"42"
+//                                  },
+//                                 @{
+//                                   @"name" : @"Joel",
+//                                   @"value": @"10"
+//                                 }
+//                               ]
+//                             }
+//                               launchOptions: nil];
+//    UIViewController *vc = [[UIViewController alloc] init];
+//    vc.view = rootView;
+//    [self presentViewController:vc animated:YES completion:nil];
+    
+}
 
 @end
